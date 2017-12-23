3b8b33df244c7dad55f01d0642894740b9b83be7
Ruben Cardenas <rubencg88@gmail.com>
2017-12-23 10:50:05 -0700

    Added recipe page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { AuthGuard } from './guards/auth.guard';
 import { RecipeListComponent } from './pages/recipe-list/recipe-list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { GroceryListComponent } from './pages/grocery-list/grocery-list.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
 
 const routes: Routes = [
   { path: 'recipe-list', component: RecipeListComponent, canActivate: [AuthGuard] },
+  { path: 'recipe', component: RecipeComponent, canActivate: [AuthGuard] },
   { path: 'grocery-list', component: GroceryListComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   // otherwise redirect to recipe-list
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { LoginComponent } from './login/login.component';
 import { GroceryListComponent } from './grocery-list/grocery-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: [RecipeListComponent, LoginComponent, GroceryListComponent]
+  declarations: [RecipeListComponent, LoginComponent, GroceryListComponent, RecipeComponent]
 })
 export class PagesModule { }
