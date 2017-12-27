cb38a5a3264793e8bf62db044cf39bd00758263b
Ruben Cardenas <rubencg88@gmail.com>
2017-12-26 19:21:22 -0700

    Added dynamic binding to grocery list

--- src/app/pages/grocery-list/grocery-list.component.ts
+++ src/app/pages/grocery-list/grocery-list.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroceryListComponent implements OnInit {
 
+  groceryGroup: any[] = [
+    { id: 1, name: "Frutas", quantityNeeded: 4, quantityBought: 2 },
+    { id: 2, name: "Verduras", quantityNeeded: 8, quantityBought: 5 },
+    { id: 3, name: "Lacteos", quantityNeeded: 6, quantityBought: 8 },
+    { id: 4, name: "Carnes", quantityNeeded: 43, quantityBought: 43 },
+    { id: 5, name: "Especias", quantityNeeded: 5, quantityBought: 3 },
+    { id: 6, name: "Otros", quantityNeeded: 14, quantityBought: 13 },
+  ];
+
   constructor() { }
 
   ngOnInit() {
