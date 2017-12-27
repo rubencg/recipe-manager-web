bb76ee396370032cbe2de0513e124c12624821de
Ruben Cardenas <rubencg88@gmail.com>
2017-12-26 17:38:56 -0700

    Added responsiveness to recipe page

--- src/app/pages/recipes/recipes.component.ts
+++ src/app/pages/recipes/recipes.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
+  recipes: any[] = [
+    { name: "Curry de garbanzos" },
+    { name: "Ensalada de Quinoa" },
+    { name: "Parfait de guayaba" },
+    { name: "Smoothie de pepino" },
+    { name: "Ragu de Res" },
+    { name: "Pizza vegana" },
+    { name: "Tu cara de res" },
+  ]
+
   constructor() { }
 
   ngOnInit() {
