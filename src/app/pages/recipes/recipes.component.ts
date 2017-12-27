af897b48728ced22c39e9b399b481f23a1832da3
Ruben Cardenas <rubencg88@gmail.com>
2017-12-26 19:05:26 -0700

    Added binding from recipe search to recipe itself

--- src/app/pages/recipes/recipes.component.ts
+++ src/app/pages/recipes/recipes.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class RecipesComponent implements OnInit {
 
   recipes: any[] = [
-    { name: "Curry de garbanzos" },
-    { name: "Ensalada de Quinoa" },
-    { name: "Parfait de guayaba" },
-    { name: "Smoothie de pepino" },
-    { name: "Ragu de Res" },
-    { name: "Pizza vegana" },
-    { name: "Tu cara de res" },
+    { id: 1, name: "Curry de garbanzos" },
+    { id: 2, name: "Ensalada de Quinoa" },
+    { id: 3, name: "Parfait de guayaba" },
+    { id: 5, name: "Smoothie de pepino" },
+    { id: 14, name: "Ragu de Res" },
+    { id: 15, name: "Pizza vegana" },
+    { id: 16, name: "Tu cara de res" },
   ]
 
   constructor() { }
