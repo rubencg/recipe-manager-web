715e50d9fca82f80d280df9c10b211ecec1b0174
Ruben Cardenas <rubencg88@gmail.com>
2017-12-26 19:47:56 -0700

    Added recipe service to recipes page

--- src/app/pages/recipes/recipes.component.ts
+++ src/app/pages/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe, RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,19 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
-  recipes: any[] = [
-    { id: 1, name: "Curry de garbanzos" },
-    { id: 2, name: "Ensalada de Quinoa" },
-    { id: 3, name: "Parfait de guayaba" },
-    { id: 5, name: "Smoothie de pepino" },
-    { id: 14, name: "Ragu de Res" },
-    { id: 15, name: "Pizza vegana" },
-    { id: 16, name: "Tu cara de res" },
-  ]
+  recipes: Recipe[]= [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getAllRecipes();
   }
 
 }
