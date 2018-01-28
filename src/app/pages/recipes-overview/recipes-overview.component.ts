d5a29d78294ffc14193bdec1d011e2ccd0f77ff6
Rubén Cárdenas <RCardenas@newgistics.com>
2018-01-28 12:20:51 -0600

    Added filter for time in recipes

--- src/app/pages/recipes-overview/recipes-overview.component.ts
+++ src/app/pages/recipes-overview/recipes-overview.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe, RecipeService } from '../../services/recipe.service';
+import { FoodTime } from '../../models/interfaces';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-recipes-overview',
@@ -9,12 +11,45 @@ import { Recipe, RecipeService } from '../../services/recipe.service';
 export class RecipesOverviewComponent implements OnInit {
 
   recipes: Recipe[]= [];
+  filteredRecipes: Recipe[]= [];
+  timeTitle = "Hora del Dia";
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipeService.getAllRecipes().subscribe((recipes: any[]) => {
       this.recipes = recipes;
+      this.filteredRecipes = recipes;
     })
+  }
+
+  selectTime(foodTime: FoodTime){
+    switch (foodTime) {
+      case FoodTime.Breakfast:
+        this.timeTitle = "Desayuno";
+        break;
+      case FoodTime.AMSnack:
+        this.timeTitle = "A.M. Snack";
+        break;
+      case FoodTime.Lunch:
+        this.timeTitle = "Comida";
+        break;
+      case FoodTime.PMSnack:
+        this.timeTitle = "P.M. Snack";
+        break;
+      case FoodTime.Dinner:
+        this.timeTitle = "Cena";
+        break;
+      default:
+        this.timeTitle = "Hora del Dia";
+        break;
+    }
+
+    if(foodTime != -1){
+      this.filteredRecipes = _.filter(this.recipes, (r: Recipe) => r.foodTime == foodTime);
+    }else{
+      this.filteredRecipes = this.recipes;
+    }
+    
   }
 }
