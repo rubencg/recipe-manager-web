1dd5a60c711d8016e31942ef0f5d0b709140f361
Rubén Cárdenas <RCardenas@newgistics.com>
2018-01-29 19:53:52 -0600

    Added week recipe

---
+++ src/app/services/week-recipe.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { FoodTime, Group, Unit, FoodGroup, Difficulty, Week } from '../models/interfaces';
+
+@Injectable()
+export class WeekRecipeService {
+  weekRecipeRef: Observable<any[]>;
+  weeks: Week[];
+
+  constructor(private db: AngularFireDatabase) { 
+    this.weekRecipeRef = db.list('weekRecipes').snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+  }
+
+  getAllWeeks(){
+    this.weekRecipeRef.subscribe(weeks => this.weeks = weeks);
+    return this.weekRecipeRef;
+  }
+
+  save(week: Week){
+    this.db.list('weekRecipes').push(week);
+  }
+
+}
